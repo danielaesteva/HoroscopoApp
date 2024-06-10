c4d31456f24c23c6a14a1d8d469de47acf3c5314
danielaesteva <d.esteva@gmail.com>
2024-06-10 11:34:56 +0200

    Listado visual del horoscopo

---
+++ app/src/main/java/com/example/horoscopoapp/HoroscopeAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.horoscopoapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class HoroscopeAdapter(private val dataSet: List<Horoscope>) :
+    RecyclerView.Adapter<HoroscopeViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HoroscopeViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_horoscope, parent, false)
+
+        return HoroscopeViewHolder(view)
+    }
+
+    // Este método simplemente es para decir cuantos elementos queremos mostrar
+    override fun getItemCount(): Int {
+        return dataSet.size
+    }
+    // Este método se llama cada vez que se va a visualizar una celda,
+    // y lo utilizaremos para mostrar los datos de esa celda
+    override fun onBindViewHolder(holder: HoroscopeViewHolder, position: Int) {
+        val horoscope = dataSet[position]
+        holder.textView.text = horoscope.name
+
+    }
+
+}
+
+class HoroscopeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    val textView: TextView
+
+    init {
+        textView = view.findViewById(R.id.nameTextView)
+    }
+
+}
